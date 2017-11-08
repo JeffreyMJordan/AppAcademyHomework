7a7f082601825f1bfa13272c143f9b9345da3aec
JeffreyMJordan <jeff2016@alumni.stanford.edu>
2017-11-07 20:56:07 -0800

    Adds ajax practice

---
+++ W6D3/Ajax_practice/js/index.js
@@ -0,0 +1,17 @@
+console.log("Hello from the JavaScript console!");
+
+$.ajax({
+	type: 'GET',
+	url: 'http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b',
+	success(data){
+		console.log("Success!");
+		console.log(data);
+	},
+	error(){
+		console.log("error");
+	},
+});
+
+console.log("Ajax request sent");
+
+
